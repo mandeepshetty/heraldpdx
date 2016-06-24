359eb1e23241fe9625e270fb9d80f11d5ddc6e19
Mandeep Shetty <mandeepshetty@gmail.com>
2016-06-23 21:35:02 -0700

    Added click handler for recyclerview view holder.
    Signed-off-by: Mandeep Shetty <mandeepshetty@gmail.com>

--- app/src/main/java/droidia/com/heraldpdx/arrivalchecker/RecyclerViewAdapter.java
+++ app/src/main/java/droidia/com/heraldpdx/arrivalchecker/RecyclerViewAdapter.java
@@ -1,10 +1,12 @@
 package droidia.com.heraldpdx.arrivalchecker;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -26,7 +28,7 @@ import timber.log.Timber;
 class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ArrivalViewHolder> {
 
     private ArrivalResults arrivals;
-
+    Context context;
     RecyclerViewAdapter(ArrivalResults arrivals) {
         this.arrivals = arrivals;
     }
@@ -77,8 +79,8 @@ class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.Arriv
         return arrivals.resultSet.arrival.size();
     }
 
-
-    static class ArrivalViewHolder extends RecyclerView.ViewHolder {
+    // todo make class static.
+    class ArrivalViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
         @BindView(R.id.estimatedArrival) TextView estimatedArrivalIn;
         @BindView(R.id.arrivalDescription) TextView arrivalDescription;
@@ -88,6 +90,10 @@ class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.Arriv
             ButterKnife.bind(this, itemView);
         }
 
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(context, "", Toast.LENGTH_SHORT).show();
+        }
     }
 }
 
